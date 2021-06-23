92e2572f330d5c6141e3efe45b2e38bfcc53cd47
Ademir Mendonça <ademir_mendonca@hotmail.com>
2021-06-23 16:15:01 -0300

    Add project files.

---
+++ PUM_URI1142/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PUM_URI1142
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int primeiro = 1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                int segundo = primeiro + 1;
+                int terceiro = primeiro + 2;
+
+                Console.WriteLine(primeiro + " " + segundo + " " + terceiro +  " PUM");
+
+                primeiro = primeiro + 4;
+            }
+        }
+    }
+}
